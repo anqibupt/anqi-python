beacdb4fbf0fb68438e21dad962064de3908de24
anqi <alexanderfs@outlook.com>
2016-04-10 22:16:45 +0800

    test user name info and the amend option

--- anqi1.py
+++ anqi1.py
@@ -1,4 +1,4 @@
-china = ['beijing', 'tianjin', ['chuzhou', 'hefei']]
+china = ['beijing', 'tianjin', ['chuzhou', 'hefei'], 'shanghai', 'guangdong']
 if isinstance(china, list):
     print(china)
 for place in china:
